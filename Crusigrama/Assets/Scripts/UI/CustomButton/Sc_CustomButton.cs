c4ae6fbfc29ff9a7a54e8316f33bf18406ac8105
Joaquin De Losada <montainproductions@gmail.com>
2024-01-05 11:12:29 -0500

    Testing Ideas

--- Crusigrama/Assets/Scripts/UI/CustomButton/Sc_CustomButton.cs
+++ Crusigrama/Assets/Scripts/UI/CustomButton/Sc_CustomButton.cs
@@ -1,18 +1,63 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.Events;
+using UnityEngine.UI;
+using System.Threading;
+using Unity.VisualScripting;
 
-public class Sc_CustomButton : MonoBehaviour
+public class Sc_CustomButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerDownHandler, IPointerClickHandler
 {
-    // Start is called before the first frame update
-    void Start()
+
+    [SerializeField]
+    private string menuGroup;
+
+    [SerializeField]
+    private Image buttonBackground;
+    [SerializeField]
+    private Vector3 startVector, enteredVector, downVector;
+    [SerializeField]
+    private Color startColor, enteredColor, downColor;
+
+    [System.Serializable]
+    public class CustomUIEvent : UnityEvent { }
+    public CustomUIEvent OnEvent;
+
+    IEnumerator Transition(Vector3 newSize, Color newColor, float transitionTime)
     {
-        
+        float timer = 0;
+        Vector3 startSize = transform.localScale;
+        Color startColor = buttonBackground.color;
+        while (timer < transitionTime)
+        {
+            timer += Time.deltaTime;
+            yield return null;
+            transform.localScale = Vector3.Lerp(startSize, newSize, timer / transitionTime);
+            buttonBackground.color = Color.Lerp(startColor, newColor, timer / transitionTime);
+        }
+        yield return null;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnPointerEnter(PointerEventData eventData)
     {
-        
+        StartCoroutine(Transition(enteredVector, enteredColor, 0.15f));
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        StartCoroutine(Transition(startVector, startColor, 0.15f));
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        StartCoroutine(Transition(downVector, downColor, 0.3f));
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        StartCoroutine(Transition(startVector, startColor, 0.02f));
+        OnEvent.Invoke();
+        Debug.Log("UI Button Clicked");
     }
 }
